468bb9e727122d174993fcd61827b567d132dee3
hoangnam150303 <hoangnam150303@gmail.com>
2025-11-18 01:33:43 +0700

    fix db

--- BE/src/middlewares/rate_limit.js
+++ BE/src/middlewares/rate_limit.js
@@ -1,5 +1,5 @@
 const { rateLimit } = require("express-rate-limit");
-export const limiter = rateLimit({
+exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 100,
   standardHeaders: "draft-8",
